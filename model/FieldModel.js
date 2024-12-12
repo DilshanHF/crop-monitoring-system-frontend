42c6510ea1766c399d460da04cb5feb975939f75
DILSHAN <dilshanfonseka76@gmail.com>
2024-12-12 23:01:09 +0530

    vehicle controller added

---
+++ model/FieldModel.js
@@ -0,0 +1,13 @@
+export default class fieldModel {
+    constructor(fieldCode,fieldLocation,fieldName,fieldSize,image1,image2,logCode,staffIds) {
+        this.fieldCode = fieldCode;
+        this.fieldName = fieldName;
+        this.fieldLocation = fieldLocation;
+        this.fieldSize = fieldSize;
+        this.image1 = image1;
+        this.image2 = image2;
+        this.logCode = logCode;
+        this.staffIds = staffIds;
+    }
+
+}
